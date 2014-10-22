b4bf307966f1bef9380984734170599c2d636e12
Haewon Hwang <hh474@cornell.edu>
2014-10-22 19:40:42 -0400

    full redesign

---
+++ js/script_schedule.js
@@ -0,0 +1,11 @@
+$('.toggleButton').click(function(){
+	$('#toggleBox').slideToggle('slow');
+});
+
+$('.toggleButton2').click(function(){
+	$('#toggleBox2').slideToggle('slow');
+});
+
+$('.toggleButton3').click(function(){
+	$('#toggleBox3').slideToggle('slow');
+});
